1120a852d578b35f7994da925846385f6262717b
Yegor Gaikov <yegorgaikov@yandex.ru>
2020-12-23 02:47:00 +0300

    add Spring Security - registration, authorization

--- src/main/java/com/example/sweater/controller/MainController.java
+++ src/main/java/com/example/sweater/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.example.sweater;
+package com.example.sweater.controller;
 
 
 import com.example.sweater.domain.Message;
@@ -14,28 +14,24 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
-public class GreetingController {
+public class MainController {
 
     @Autowired
     private MessageRepo messageRepo;
 
-    @GetMapping("/greeting")
-    public String greeting(
-            @RequestParam(name = "name", required = false, defaultValue = "World") String name,
-            Map<String, Object> model
-    ) {
-        model.put("name", name);
+    @GetMapping("/")
+    public String greeting(Map<String, Object> model) {
         return "greeting";
     }
 
-    @GetMapping
+    @GetMapping("/main")
     public String main(Map<String, Object> model) {
         Iterable<Message> messages = messageRepo.findAll();
         model.put("messages", messages);
         return "main";
     }
 
-    @PostMapping
+    @PostMapping("/main")
     public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
         Message message = new Message(text, tag);
         messageRepo.save(message);
@@ -50,8 +46,8 @@ public class GreetingController {
     public String filter(@RequestParam String filter, Map<String, Object> model) {
         Iterable<Message> messages;
 
-        if(filter != null && !filter.isEmpty()){
-          messages = messageRepo.findByTagContaining(filter);
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTagContaining(filter);
         } else {
             messages = messageRepo.findAll();
         }
